ba07c537d43aee35f8c95a0ae5807901061cca0a
Abdul <aabdulmajeed.isa@gmail.com>
2023-08-27 00:12:48 +0100

    pycodestyle clean up

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 import math
 
-# list(filter(lambda x: is_quadratic_residue(87463, x),sieve_of_eratosthenes(43)))
-def trial_division(n, lst = []):
+
+def trial_division(n, lst=[]):
     c = 0
     if n > 1:
         if lst:
@@ -11,7 +11,7 @@ def trial_division(n, lst = []):
                     break
                 c += 1
             if c == len(lst):
-                return None    
+                return None
             return p, q
         else:
             for q in range(2, (n//2)+1):
@@ -19,7 +19,8 @@ def trial_division(n, lst = []):
                     p = n // q
                     break
             return p, q
-    
+
+
 def b_smooth(N):
     """Smoothness bound formula"""
 
@@ -27,48 +28,52 @@ def b_smooth(N):
     b_smooth = L ** (1 / math.sqrt(2))
     return math.ceil(b_smooth)
 
+
 def sieve_of_eratosthenes(limit):
     """Sieve of Eratosthenes"""
     primes = []
     is_prime = [True] * (limit + 1)
     is_prime[0] = is_prime[1] = False
-    
+
     for num in range(2, int(limit**0.5) + 1):
         if is_prime[num]:
             primes.append(num)
-            is_prime[num*num : limit+1 : num] = [False] * len(range(num*num, limit+1, num))
-    
+            length = len(range(num*num, limit+1, num))
+            is_prime[num*num: limit+1: num] = [False] * length
+
     for num in range(int(limit**0.5) + 1, limit + 1):
         if is_prime[num]:
             primes.append(num)
     return primes
 
-"""Euler's Criteria for Quadratic Residues"""
-def is_quadratic_residue(n, p):
+
+def is_quad_residue(n, p):
+    """Euler's Criteria for Quadratic Residues"""
     if n % p == 0:
         return True  # 0 is a quadratic residue modulo any prime
     return pow(n, (p - 1) // 2, p) == 1
 
-"""Fermat's factorization method"""
+
 def fermat_factorization(n):
+    """Fermat's factorization method"""
     limit = 1000
     c = 0
 
-    if(n <= 0):
-        return [n] 
- 
-    if(n and 1) == 0: 
+    if n <= 0:
+        return [n]
+
+    if n and 1 == 0:
         return [n / 2, 2]
-         
+
     a = math.ceil(math.sqrt(n))
- 
-    if(a * a == n):
+
+    if a * a == n:
         return [a, a]
- 
-    while(c != limit):
+
+    while c != limit:
         b1 = a * a - n
         b = int(math.sqrt(b1))
-        if(b * b == b1):
+        if (b * b == b1):
             break
         else:
             a += 1
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from prime import isprime
 from func import *
 
+
 def main():
     n = len(sys.argv)
     if n == 2:
@@ -10,9 +11,9 @@ def main():
         for line in lines:
             N = int(line.strip())
             B = b_smooth(N)
-            base_factors = sieve_of_eratosthenes(B + 1)
-            r_base_factors =  list(filter(lambda x: is_quadratic_residue(N, x), base_factors))
-            factors = trial_division(N, r_base_factors)
+            base_factors = sieve_of_eratosthenes(B+1)
+            rbf = list(filter(lambda x: is_quad_residue(N, x), base_factors))
+            factors = trial_division(N, rbf)
             if not factors:
                 factors = fermat_factorization(N)
             p, q = factors
